40d725b50cf7ed26fee7e45651b0cb67b02e5a92
Madushankumarasiri <61668132+Madushankumarasiri@users.noreply.github.com>
2021-08-26 22:25:59 +0530

    adding styles for expense component

--- Frontend/src/Components/Expense.jsx
+++ Frontend/src/Components/Expense.jsx
@@ -1,7 +1,9 @@
 const Expense = ({expense}) => {
     return ( 
-       <div>
-           <p>{expense.description}</p>
+       <div className="item">
+           <p className="description">{expense.description}</p>
+           <small className="text-muted">{expense.date}</small>
+           <h6 className="item-value">${expense.expenseValue}</h6>
        </div>
      );
 }
