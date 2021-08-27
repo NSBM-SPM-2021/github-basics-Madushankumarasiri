012890e951fc7ba9c2fbaebeed3ebb1e51a86028
Madushankumarasiri <61668132+Madushankumarasiri@users.noreply.github.com>
2021-08-28 01:04:59 +0530

    Setup azure KeyVault for secrets

--- Backend/ExpenseTrackerAPI/Program.cs
+++ Backend/ExpenseTrackerAPI/Program.cs
@@ -1,3 +1,6 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +21,19 @@ namespace ExpenseTrackerAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var builtConfiguration = config.Build();
+
+                    string kvURL = builtConfiguration["KeyVaultConfig:KVUrl"];
+                    string tenantId = builtConfiguration["KeyVaultConfig:TenantId"];
+                    string clientId = builtConfiguration["KeyVaultConfig:ClientId"];
+                    string clientSecret = builtConfiguration["KeyVaultConfig:ClientSecretId"];
+
+                    var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+                    var client = new SecretClient(new Uri(kvURL), credential);
+                    config.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
