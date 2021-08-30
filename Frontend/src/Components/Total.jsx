f0ee31a0a46b4d5730aecdec1a3fc2d6e70c1b23
Madushankumarasiri <61668132+Madushankumarasiri@users.noreply.github.com>
2021-08-30 07:35:30 +0530

    Revert "Setup firebase"
    This reverts commit 8c0276ab674aaa4f64c31b970c69ecb527b7ada7.

--- Frontend/src/Components/Total.jsx
+++ Frontend/src/Components/Total.jsx
@@ -2,7 +2,7 @@ const Total = () => {
     return ( 
         <div className="total">
             <h6 className="total-heading">My Total</h6>
-            <h5 className="total-value">-</h5>
+            <h5 className="total-value">Rs 3000</h5>
             <hr/>
         </div>
      );
